bacd3adc40ef85de2174f8a1f8d5d71f1c867366
cris25nkl <cristian25florez@gmail.com>
2025-12-03 11:17:37 -0500

    Calendar

--- app/src/main/java/com/example/agenda_kotlin/data/Converters.kt
+++ app/src/main/java/com/example/agenda_kotlin/data/Converters.kt
@@ -1,16 +1,1 @@
-package com.example.agenda_kotlin.data
 
-import androidx.room.TypeConverter
-import com.example.agenda_kotlin.model.Prioridad
-
-class Converters {
-    @TypeConverter
-    fun fromPrioridad(prioridad: Prioridad): String {
-        return prioridad.name
-    }
-    
-    @TypeConverter
-    fun toPrioridad(prioridad: String): Prioridad {
-        return Prioridad.valueOf(prioridad)
-    }
-}
